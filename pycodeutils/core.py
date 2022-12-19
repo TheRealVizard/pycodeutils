8ca736fda5f2118f29b0986ab49c89826af584b6
TheRealVizard <vizard@divineslns.com>
2022-12-19 13:39:06 -0500

    Added set

--- pycodeutils/core.py
+++ pycodeutils/core.py
@@ -1,12 +1,28 @@
+from collections.abc import Iterable
+
+
 class list(list):
     def mapped(self, function):
         return list(map(function, self))
 
     def _list_split(self, size, step):
-        return list(self[i:i+size] for i in range(0, len(self), step) if size +i <= len(self))
+        return self(
+            self[i : i + size]
+            for i in range(0, len(self), step)
+            if size + i <= len(self)
+        )
 
     def chunked(self, size):
         return self._list_split(size, size)
 
-    def window(self,size):
+    def window(self, size):
         return self._list_split(size, 1)
+
+
+class set(set):
+    def __add__(self, element) -> set:
+        if type(element) == set:
+            return set(self.union(element))
+        elif isinstance(element, Iterable):
+            return set(self.union(set(element)))
+        return set(self.union(set([element])))
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,11 +1,20 @@
+from unittest import TestCase
+
 from pycodeutils import *
 
-from unittest import TestCase
 
 class Test(TestCase):
     def setUp(self) -> None:
         self.list = list([1, 2, 3, 4, 5])
+        self.set = set([1, 2, 3, 4])
 
     def test_map(self):
-        print(self.list.mapped(lambda x: x + 1))
         self.assertListEqual(self.list.mapped(lambda x: x + 1), [2, 3, 4, 5, 6])
+
+    def test_set(self):
+        new_set = self.set + [1, 5, 6, 7]
+        self.assertEqual(len(new_set), 7)
+        new_set = new_set + set([8, 6, 7])
+        self.assertEqual(len(new_set), 8)
+        new_set = self.set + 10
+        self.assertEqual(len(new_set), 5)
